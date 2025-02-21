2f8d974536772ef8501ed8fe4ba6cb77495f42d0
Gurushesh-Metapercept <gurushesh.p@metapercept.com>
2025-02-21 17:44:04 +0530

    git branch diff

--- src/components/GithubAPI.jsx
+++ src/components/GithubAPI.jsx
@@ -1,6 +1,5 @@
 import GitComparison from "./GitComparison";
-const token =
-  "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";
+const token = "";
 
 const GithubAPI = () => {
   return (
